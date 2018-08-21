d9203fb8956191d8af42e6038dfc62fd59683b2e
“karimshakir” <karimshakir99@gmail.com>
2018-08-20 21:49:14 -0500

    first commit

---
+++ store_item.rb
@@ -0,0 +1,36 @@
+shoe_1 = { name: "yeezy", color: "black", cost: 150 }
+options = { font_size: 10, font_family: "Arial" }
+shoe_2 = { name: "jordan", color: "red", cost: 200 }
+shoe_3 = {:name => "yeezy", :color => "black", :cost => 150}
+
+puts "#{shoe_3[:color]}  #{shoe_3[:name]} cost #{shoe_3[:cost]}"
+
+
+class Shoes
+  def initialize(name, color, cost)
+    @name = name
+    @color = color
+    @cost = cost
+  end
+
+  def name
+    @name
+  end
+
+  def color
+    @color
+  end
+
+  def cost
+    @cost
+  end
+
+  def print_out
+      p "name: #{name}, color: #{color}, cost: #{cost}"
+  end
+
+end
+
+my_shoes = Shoes.new("new_balance", "royal", "$60")
+my_shoes.print_out
+
